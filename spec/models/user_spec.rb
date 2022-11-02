5425686ca9b9a88f05b449d2c4be9303180e9c60
tyo6ra6 <maako527@icloud.com>
2022-11-02 15:37:29 +0900

    モデル単体テストコード

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できる場合' do
+      it 'nicknameとemail、passwordとpassword_confirmation、sex_idとgeneration_id、heightが存在すれば登録できる' do
+        expect(@user).to be_valid
+      end
+    end
+    context '新規登録できない場合' do
+      it 'nicknameが空では登録できない' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it 'emailが空では登録できない' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it 'passwordが空では登録できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it 'passwordとpassword_confirmationが不一致では登録できない' do
+        @user.password_confirmation = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it '重複したemailが存在する場合は登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user, email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
+      end
+      it 'emailに@を含まないと登録できない' do
+        @user.email = 'testmail'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')     
+      end
+      it 'passwordが5文字以下では登録できない' do
+        @user.password = '00000'
+        @user.password_confirmation = '00000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
+      it 'sex_idが空だと保存できない  ' do
+        @user.sex_id = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Sex is not a number")
+      end    
+      it 'generation_idが空だと保存できない' do
+        @user.generation_id = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Generation is not a number")
+      end    
+      it 'heightが空では登録できない' do
+        @user.height = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Height can't be blank")
+      end
+    end
+  end
+end
