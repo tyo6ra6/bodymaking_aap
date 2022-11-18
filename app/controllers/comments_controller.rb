57b54f65f9bea93ee3a81860998e7ff920ce1bca
tyo6ra6 <maako527@icloud.com>
2022-11-18 19:56:46 +0900

    rubcop

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,17 +2,17 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to record_path(@comment.record) 
+      redirect_to record_path(@comment.record)
     else
       @record = @comment.record
       @comments = @record.comments
-      render "records/show" 
+      render 'records/show'
     end
-  end 
+  end
 
   private
 
-   def  comment_params
+  def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, record_id: params[:record_id])
-   end 
+  end
 end
